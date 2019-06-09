1b28404e5798b5c5d893c276d6c303284eeeca4e
Saimadhav Heblikar <sahutd@users.noreply.github.com>
2019-06-09 15:29:43 +0530

    reformat code

--- joystickDriver.c
+++ joystickDriver.c
@@ -1,98 +1,11 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
 #include <linux/usb.h>
 #include <linux/slab.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/netlink.h>
 
-#include <net/netlink.h>
-#include <net/net_namespace.h>
-
-MODULE_LICENSE("GPL");
-
-struct urb* my_urb;
-void* my_buffer;
-static struct usb_interface* my_interface;
-static const struct usb_device_id* my_id;
-static int pen_probe(struct usb_interface*, const struct usb_device_id*);
-
-struct controller
-{
-    char xaxis;
-    char yaxis;
-    char updownaxis;
-    char b1;
-    char b2;
-    char b3;
-    char b4;
-};
-
-static struct usb_device_id pen_table[] = {
-
-    { USB_DEVICE(0x0583, 0xb03b) }, {},
-};
-
-/* Protocol family, consistent in both kernel prog and user prog. */
-#define MYPROTO NETLINK_USERSOCK
-/* Multicast group, consistent in both kernel prog and user prog. */
-#define MYGRP 31
-
-static struct sock* nl_sk = NULL;
-
-static void send_netlink_message(void)
-{
-    struct sk_buff* skb;
-    struct nlmsghdr* nlh;
-    char* msg = "Hello from kernel";
-    int msg_size = strlen(msg) + 1;
-    int res;
-
-    pr_info("Creating skb.\n");
-    skb = nlmsg_new(NLMSG_ALIGN(msg_size + 1), GFP_KERNEL);
-    if (!skb)
-    {
-        pr_err("Allocation failure.\n");
-        return;
-    }
-
-    nlh = nlmsg_put(skb, 0, 1, NLMSG_DONE, msg_size + 1, 0);
-    strcpy(nlmsg_data(nlh), msg);
-    pr_info("Sending skb.\n");
-    res = nlmsg_multicast(nl_sk, skb, 0, MYGRP, GFP_KERNEL);
-    if (res < 0)
-        pr_info("nlmsg_multicast() error: %d\n", res);
-    else
-        pr_info("Success.\n");
-}
-
-void myHandler(struct urb* urb)
-{
-    char* buffer = (char*)urb->transfer_buffer;
-    struct controller ontroller;
-
-    ontroller.xaxis = buffer[0];
-    ontroller.yaxis = buffer[1];
-    ontroller.updownaxis = buffer[2];
-    ontroller.b1 = buffer[3];
-    ontroller.b2 = buffer[4];
-    ontroller.b3 = buffer[5];
-    ontroller.b4 = buffer[6];
-
-    if (ontroller.b1 + ontroller.b2 + ontroller.b3 + ontroller.b4 > 0)
-    {
-        send_netlink_message();
-    }
-    usb_submit_urb(urb, GFP_ATOMIC);
-}
-static struct usb_device* device;
-#include <linux/module.h>
-#include <linux/kernel.h>
-#include <linux/usb.h>
-#include <linux/slab.h>
-
-
-#include <linux/module.h>
-#include <linux/kernel.h>
-#include <linux/netlink.h>
 #include <net/netlink.h>
 #include <net/net_namespace.h>
 
