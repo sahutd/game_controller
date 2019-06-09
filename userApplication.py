adc52f8d6107a5b7e37a6677f85888ad4456ce13
Saimadhav Heblikar <sahutd@users.noreply.github.com>
2019-06-09 20:41:25 +0530

    Added scales for 3 axis input

--- userApplication.py
+++ userApplication.py
@@ -5,8 +5,10 @@ import string
 import enum
 import threading
 import tkinter
+from tkinter import ttk
 
 BUTTONCOUNT = 4
+AXISCOUNT = 3
 
 class ButtonStatus(enum.Enum):
   OFF = 0
@@ -26,22 +28,45 @@ class Button:
   def __repr__(self):
     return 'Button:{} status:{}'.format(self.id_, self.status)
 
+class Axis:
+  def __init__(self, id_):
+    self.id = id_
+    self.status = 0
+
+  def set(self, status):
+    self.status = status
+  
+  def __repr__(self):
+    return 'Axis:{} status:{}'.format(self.id_, self.status)
+
 
 class Joystick:
   def __init__(self):
     self.buttons = [Button(i) for i in range(BUTTONCOUNT)]
+    self.axes = [Axis(i) for i in range(AXISCOUNT)]
 
   def __repr__(self):
     return ' '.join(str(button) for button in self.buttons)
 
   def update(self, string):
     temp = string.split(',')
-    buttonString = temp[0][:4]
+    buttonString, axesStrings = temp[0], temp[1:]
+
+    self.updateButtons(buttonString[:4])
+    self.updateAxes(axesStrings)
+  
+  def updateButtons(self, buttonString):   
     for i, s in enumerate(buttonString):
         if s == '1':
           self.buttons[i].set()
         if s== '0':
           self.buttons[i].unset()
+
+  def updateAxes(self, axesStrings):
+    print(axesStrings)
+    for i, a in enumerate(axesStrings):
+      self.axes[i].set(int(a))
+      
 
 
 class Gui:
@@ -50,13 +75,27 @@ class Gui:
     
     self.booleanVar = {}
     self.window = tkinter.Tk()
+    self.axesLabels = {}
+    self.setupWidgets()
 
+  def setupWidgets(self):
+    self.setupButtons()
+    self.setupAxes()
 
-    for button in joystick.buttons:
+  def setupButtons(self):
+    for button in self.joystick.buttons:
       self.booleanVar[button] = tkinter.Checkbutton(self.window, text='Button {}'.format(button.id_))
       self.booleanVar[button].pack()
-
-
+  
+  def setupAxes(self):
+    for a in self.joystick.axes:
+      if a.id == 0:
+        orient = tkinter.HORIZONTAL
+      else:
+        orient = tkinter.VERTICAL
+      tkinter.Label(self.window, text='Axis{}'.format(a.id)).pack()
+      self.axesLabels[a] = ttk.Scale(self.window, orient=orient, from_=0, to=255)
+      self.axesLabels[a].pack()
       
     self.update()
   def run(self):
@@ -64,12 +103,20 @@ class Gui:
 
 
   def update(self):
+    self.updateButtons()
+    self.updateAxes()
+
+  def updateButtons(self):
     for button in self.joystick.buttons:
       if button.status == ButtonStatus.ON:
         self.booleanVar[button].select()
       else:
         self.booleanVar[button].deselect()
 
+  def updateAxes(self):
+    
+    for axes in self.joystick.axes:
+      self.axesLabels[axes]['value'] = axes.status
 
 
 class clientThread(threading.Thread):
@@ -87,17 +134,16 @@ class clientThread(threading.Thread):
     allowed = set(','+string.digits)
 
     def parse(string):
-      return ''.join(s for s in string.decode() if s in allowed)
-      
+      return ''.join(s for s in string.decode() if s in allowed)      
 
     while 1:
       try:       
-        msg = sock.recvfrom(1024)[0]
-        new_msg = parse(msg)
-        self.joystick.update(new_msg)
+        msg = parse(sock.recvfrom(1024)[0])
       except Exception as e:
         print(e)
+      self.joystick.update(msg)
       self.gui.update()
+      
 
 if __name__ == '__main__':
   joystick = Joystick()
